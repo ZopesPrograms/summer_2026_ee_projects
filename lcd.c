e7bdd24995dc24281a2487b6e05264af88a3cb43
ZopesPrograms <zopesconk1@gmail.com>
2026-07-05 00:34:34 -0700

    Used Claude Sonnet from Stanford AI playground to help debug code with ILI9486 data sheet and cs107e library as reference; still no display unfortunately.

--- lcd.c
+++ lcd.c
@@ -9,68 +9,81 @@ static spi_device_t *lcd;
 
 static const uint8_t COL_ADDR_SET  = 0x2A; // Sets column (x) address for pixel to be written or read
 static const uint8_t PAGE_ADDR_SET = 0x2B; // Sets page (y) address for pixel to be written or read
-
 static const uint8_t RAM_WR = 0x2C; // RAM WRITE to LCD display memory
-/* static const uint8_t RAM_RD = 0x2E; // RAM READ from LCD display memory */
+
+// Small delay to ensure CS timing is met
+static void cs_delay(void) {
+    timer_delay_us(1);  // 1 microsecond delay
+}
 
 void lcd_init(int16_t width, int16_t height) {
     // Initializes LCD screen as SPI device
-    lcd = spi_new(CS_PIN, SPI_MODE_0, 1000000);
+    lcd = spi_new(CS_PIN, SPI_MODE_0, 4000000);  // 4 MHz - conservative speed
 
     // Assigns static width and height of LCD pixel-map/output to requested values:
     _width = width;
     _height = height;
 
-    // Configures pins we are using (and one we are not, the flash chip select pin) to direct LED to be in output mode!
+    // Configures pins we are using
     gpio_set_output(F_CS_PIN);
     gpio_set_output(RS_PIN);
     gpio_set_output(RST_PIN);
 
-    // Toggle RST high then low to reset
+    // Initial pin states - deselect flash chip
+    gpio_write(F_CS_PIN, 1);
+    gpio_write(RS_PIN, 1);
+    gpio_write(CS_PIN, 1);
+
+    // Hardware reset sequence
     gpio_write(RST_PIN, 1);
-
-    // Sets up default values of chip-select data/command select pins when lcd not doing anything:
-    gpio_write(RS_PIN, 1);   // Default data/command select setting
-    gpio_write(CS_PIN, 1);   // Default chip select setting
-    gpio_write(F_CS_PIN, 1); // Default flash chip select setting (so it doesn't float and do weird stuff).
-
-    timer_delay_ms(50);
-
+    timer_delay_ms(5);
     gpio_write(RST_PIN, 0);
-    timer_delay_ms(10);
-    
+    timer_delay_ms(20);
     gpio_write(RST_PIN, 1);
-    timer_delay_ms(10);
+    timer_delay_ms(150);
 
-    // Resets lcd software + all previous commands! Sets everything to default params.
+    // Software reset
     lcd_writecommand(LCD_RESET_SOFT);
-    lcd_writedata(0x00);
-    timer_delay_ms(50);
+    timer_delay_ms(120);
 
-    // Turns display off for setup.
-    lcd_writecommand(LCD_DISPLAY_OFF);
-    lcd_writedata(0x00);
+    // Sleep out
+    lcd_writecommand(LCD_SLEEP_OUT);
+    timer_delay_ms(120);
 
+    // Interface Pixel Format - 16-bit color (RGB565)
+    lcd_writecommand(INTF_PIX_FORM);
+    lcd_writedata(0x55);
+
+    // Power Control 1
     lcd_writecommand(LCD_POWER_CTRL_1);
     lcd_writedata(0x0d);
     lcd_writedata(0x0d);
 
+    // Power Control 2
     lcd_writecommand(LCD_POWER_CTRL_2);
     lcd_writedata(0x43);
     lcd_writedata(0x00);
 
+    // Power Control 3
     lcd_writecommand(LCD_POWER_CTRL_3);
     lcd_writedata(0x00);
 
+    // VCOM Control
     lcd_writecommand(VCOM_CTRL);
     lcd_writedata(0x00);
     lcd_writedata(0x48);
 
+    // Memory Access Control
+    lcd_writecommand(MEM_ACC_CTRL);
+    lcd_writedata(0x48);  // Sets rotation and BGR order
+
+    // Display Function Control
     lcd_writecommand(DISP_FUNC_CTRL);
     lcd_writedata(0x00);
     lcd_writedata(0x22);
-    lcd_writedata(0x3b);
+    lcd_writedata(0x3B);  // 480 lines
 
+    // Positive Gamma Control
     lcd_writecommand(POS_GAMMA_CTRL);
     lcd_writedata(0x0f);
     lcd_writedata(0x24);
@@ -88,6 +101,7 @@ void lcd_init(int16_t width, int16_t height) {
     lcd_writedata(0x07);
     lcd_writedata(0x00);
 
+    // Negative Gamma Control
     lcd_writecommand(NEG_GAMMA_CTRL);
     lcd_writedata(0x0f);
     lcd_writedata(0x38);
@@ -105,77 +119,105 @@ void lcd_init(int16_t width, int16_t height) {
     lcd_writedata(0x1b);
     lcd_writedata(0x00);
 
+    // Display Inversion Off
     lcd_writecommand(DISP_INV_OFF);
-    lcd_writecommand(MEM_ACC_CTRL);
-    lcd_writedata(0x0a);
 
-    lcd_writecommand(INTF_PIX_FORM);
-    lcd_writedata(0x55);
-
-    lcd_writecommand(LCD_SLEEP_OUT);    
-    timer_delay_ms(150);
-
+    // Display On
     lcd_writecommand(LCD_DISPLAY_ON);
-    timer_delay_ms(25);
+    timer_delay_ms(100);
 }
+
 void lcd_writecommand(uint8_t c) {
-    // Start a communication by dipping to low on chip select line
     gpio_write(CS_PIN, 0);
-    // Tells LCD this communication is a COMMAND by dipping on command/data line
-    gpio_write(RS_PIN, 0);
-
-    // Writes command byte by SPI
+    cs_delay();
+    gpio_write(RS_PIN, 0);  // Command mode
     spi_write(lcd, &c, 1);
-
-    // Resets for next op
-    gpio_write(RS_PIN, 1);
-    gpio_write(CS_PIN, 1);
+    cs_delay();
+    gpio_write(CS_PIN, 1);  // Raise CS after command
+    cs_delay();
 }
 
 void lcd_writedata(uint8_t c) {
-    // Start a communication by dipping to low on chip select line
     gpio_write(CS_PIN, 0);
-
-    // Writes data byte by SPI
+    cs_delay();
+    gpio_write(RS_PIN, 1);  // Data mode
     spi_write(lcd, &c, 1);
-
-    // Resets for next op
-    gpio_write(CS_PIN, 1);
+    cs_delay();
+    gpio_write(CS_PIN, 1);  // Raise CS after data
+    cs_delay();
 }
 
 void lcd_drawpixel(uint16_t x, uint16_t y, uint16_t color) {
     // Faster range checking, possible because x and y are unsigned
     if ((x >= _width) || (y >= _height)) return;
 
-    // Command to set column/x address for pixel to be drawn
+    // Set column address (x)
     gpio_write(CS_PIN, 0);
+    cs_delay();
     gpio_write(RS_PIN, 0);
     spi_write(lcd, &COL_ADDR_SET, 1);
+    cs_delay();
+    gpio_write(CS_PIN, 1);
+    cs_delay();
 
-    // Data for column address set sent to lcd
+    // Send column start and end (big-endian)
+    uint8_t x_data[4];
+    x_data[0] = (x >> 8) & 0xFF;  // Start column MSB
+    x_data[1] = x & 0xFF;          // Start column LSB
+    x_data[2] = (x >> 8) & 0xFF;  // End column MSB
+    x_data[3] = x & 0xFF;          // End column LSB
+
+    gpio_write(CS_PIN, 0);
+    cs_delay();
     gpio_write(RS_PIN, 1);
+    spi_write(lcd, x_data, 4);
+    cs_delay();
+    gpio_write(CS_PIN, 1);
+    cs_delay();
 
-    // Parameter that identifies x position for display memory RAM to modify (sent by SPI)
-    spi_write(lcd, (uint8_t*)&x, 2);
-
-    // Command to set page/y address for pixel to be drawn
+    // Set page address (y)
+    gpio_write(CS_PIN, 0);
+    cs_delay();
     gpio_write(RS_PIN, 0);
     spi_write(lcd, &PAGE_ADDR_SET, 1);
+    cs_delay();
+    gpio_write(CS_PIN, 1);
+    cs_delay();
 
-    // Data for page address set sent to lcd
+    // Send page start and end (big-endian)
+    uint8_t y_data[4];
+    y_data[0] = (y >> 8) & 0xFF;  // Start page MSB
+    y_data[1] = y & 0xFF;          // Start page LSB
+    y_data[2] = (y >> 8) & 0xFF;  // End page MSB
+    y_data[3] = y & 0xFF;          // End page LSB
+
+    gpio_write(CS_PIN, 0);
+    cs_delay();
     gpio_write(RS_PIN, 1);
+    spi_write(lcd, y_data, 4);
+    cs_delay();
+    gpio_write(CS_PIN, 1);
+    cs_delay();
 
-    // Parameter that identifies y position for display memory RAM to modify (sent by SPI)
-    spi_write(lcd, (uint8_t*)&y, 2);
-
-    // Command to write pixel color to LCD display RAM
+    // Write pixel color to RAM
+    gpio_write(CS_PIN, 0);
+    cs_delay();
     gpio_write(RS_PIN, 0);
     spi_write(lcd, &RAM_WR, 1);
-
-    // Color data sent to LCD to be written to display RAM
-    gpio_write(RS_PIN, 1);
-    spi_write(lcd, (uint8_t*)&color, 2);
-
-    // Ends pixel draw operation by releasing chip select
+    cs_delay();
     gpio_write(CS_PIN, 1);
+    cs_delay();
+
+    // Send color data (big-endian RGB565)
+    uint8_t color_data[2];
+    color_data[0] = (color >> 8) & 0xFF;  // Color MSB
+    color_data[1] = color & 0xFF;          // Color LSB
+
+    gpio_write(CS_PIN, 0);
+    cs_delay();
+    gpio_write(RS_PIN, 1);
+    spi_write(lcd, color_data, 2);
+    cs_delay();
+    gpio_write(CS_PIN, 1);
+    cs_delay();
 }
--- main.c
+++ main.c
@@ -8,20 +8,20 @@ int main() {
     timer_init();
     uart_init();
     spi_init();
-    lcd_init(480, 320);
+    lcd_init(320, 480);
 
-    lcd_drawpixel(99, 99, 0xAC4F);
-    lcd_drawpixel(100, 99, 0xAC4F);
-    lcd_drawpixel(101, 99, 0xAC4F);
+    lcd_drawpixel(99, 99, 0x07E0);
+    lcd_drawpixel(100, 99, 0x07E0);
+    lcd_drawpixel(101, 99, 0x07E0);
 
-    lcd_drawpixel(99, 100, 0xAC4F);
-    lcd_drawpixel(100, 100, 0xAC4F);
-    lcd_drawpixel(101, 100, 0xAC4F);
+    lcd_drawpixel(99, 100, 0x07E0);
+    lcd_drawpixel(100, 100, 0x07E0);
+    lcd_drawpixel(101, 100, 0x07E0);
 
-    lcd_drawpixel(99, 101, 0xAC4F);
-    lcd_drawpixel(100, 101, 0xAC4F);
-    lcd_drawpixel(101, 101, 0xAC4F);
+    lcd_drawpixel(99, 101, 0x07E0);
+    lcd_drawpixel(100, 101, 0x07E0);
+    lcd_drawpixel(101, 101, 0x07E0);
 
-    while(1) { /* SPIN!!! */ }
+    while(1) { /*uart_putchar('a');*/ }
     return 0;
 }
